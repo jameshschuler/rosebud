4b4f319079884b3ace24156c55de507d19ed7226
jamesschuler <jameshschuler1@gmail.com>
2025-05-24 17:44:33 -0700

    FE config updates

--- swol/web/vite.config.ts
+++ swol/web/vite.config.ts
@@ -1,14 +1,18 @@
-import path from 'node:path'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(import.meta.dir, './src'),
-      '@api': path.resolve(import.meta.dir, '../api/src'),
+      '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, '../api/src'),
     },
   },
 })
