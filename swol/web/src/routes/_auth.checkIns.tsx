d2cb4fbcbb1501cead62387e5694e71de626d17c
jamesschuler <jameshschuler1@gmail.com>
2025-08-06 22:45:09 -0700

    adjusted import path

--- swol/web/src/routes/_auth.checkIns.tsx
+++ swol/web/src/routes/_auth.checkIns.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { CheckIns } from '@/features/CheckIns/components/CheckIns.tsx'
+import { CheckIns } from '../features/CheckIns/components/CheckIns'
 
 export const Route = createFileRoute('/_auth/checkIns')({
   component: CheckIns,
