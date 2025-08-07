abb03366242012167c3cdefd0489bd02cabd7437
jamesschuler <jameshschuler1@gmail.com>
2025-08-06 23:01:40 -0700

    idk

--- swol/web/src/routes/_auth.checkIns.tsx
+++ swol/web/src/routes/_auth.checkIns.tsx
@@ -2,5 +2,11 @@ import { createFileRoute } from '@tanstack/react-router'
 import { CheckIns } from '../features/CheckIns/components/CheckIns'
 
 export const Route = createFileRoute('/_auth/checkIns')({
-  component: CheckIns,
+  component: CheckInsPage,
 })
+
+function CheckInsPage() {
+  return (
+    <CheckIns />
+  )
+}
